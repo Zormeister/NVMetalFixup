63bd62299d484e424e13a281d4181f42dfacba13
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-03-21 08:15:32 +0200

    Don’t panic on error

--- NVMetalFixup/kern_nvmf.cpp
+++ NVMetalFixup/kern_nvmf.cpp
@@ -30,15 +30,13 @@ void NVMF::init() {
 }
 
 void NVMF::processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size) {
-    patcher.clearError();
-
     if (kextIOAccelFamily2.loadIndex == index) {
         KernelPatcher::SolveRequest solveRequests[] = {
             {"__ZTV18IOAccelStatistics2", this->orgIOAccelStatisticsVTable},
             {"__ZTV19IOAccelCommandQueue", this->orgIOAccelCommandQueueVTable},
             {"__ZTV22IOGraphicsAccelerator2", this->orgIOGraphicsAccelVTable},
         };
-        PANIC_COND(!patcher.solveMultiple(index, solveRequests, address, size), "nvmf",
+        SYSLOG_COND(!patcher.solveMultiple(index, solveRequests, address, size), "nvmf",
             "Failed to solve IOAcceleratorFamily2 symbols");
 
         KernelPatcher::RouteRequest requests[] = {
@@ -47,7 +45,7 @@ void NVMF::processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t a
              "jyPyj",
                 wrapNewResourceWithOptions, this->orgNewResourceWithOptions},
         };
-        PANIC_COND(!patcher.routeMultiple(index, requests, address, size), "nvmf",
+        SYSLOG_COND(!patcher.routeMultipleLong(index, requests, address, size), "nvmf",
             "Failed to route IOAcceleratorFamily2 symbols");
     } else if (kextGeForceWeb.loadIndex == index) {
         mach_vm_address_t *orgNVStatisticsVTable {nullptr}, *orgNVCommandQueueVTable {nullptr},
@@ -58,16 +56,25 @@ void NVMF::processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t a
             {"__ZTV14nvCommandQueue", orgNVCommandQueueVTable},
             {"__ZTV19nvAcceleratorParent", orgNVAccelParentVTable},
         };
-        PANIC_COND(!patcher.solveMultiple(index, solveRequests, address, size), "nvmf",
+        SYSLOG_COND(!patcher.solveMultiple(index, solveRequests, address, size, true, true), "nvmf",
             "Failed to solve GeForceWeb symbols");
 
         /**
          * Apple added a few new methods. This is fine; they added them over their _RESERVED placeholders.
          */
-        memcpy(orgNVStatisticsVTable + 39, callback->orgIOAccelStatisticsVTable + 39, sizeof(mach_vm_address_t) * 4);
-        memcpy(orgNVCommandQueueVTable + 325, callback->orgIOAccelCommandQueueVTable + 325,
+        if (orgNVStatisticsVTable && callback->orgIOAccelStatisticsVTable) {
+            DBGLOG("nvmf", "Patching nvStatistics VTable");
+            memcpy(orgNVStatisticsVTable + 39, callback->orgIOAccelStatisticsVTable + 39, sizeof(mach_vm_address_t) * 4);
+        }
+        if (orgNVCommandQueueVTable && callback->orgIOAccelCommandQueueVTable) {
+            DBGLOG("nvmf", "Patching nvCommandQueue VTable");
+            memcpy(orgNVCommandQueueVTable + 325, callback->orgIOAccelCommandQueueVTable + 325,
             sizeof(mach_vm_address_t) * 2);
-        memcpy(orgNVAccelParentVTable + 340, callback->orgIOGraphicsAccelVTable + 340, sizeof(mach_vm_address_t));
+        }
+        if (orgNVAccelParentVTable && callback->orgIOGraphicsAccelVTable) {
+            DBGLOG("nvmf", "Patching nvAcceleratorParent VTable");
+            memcpy(orgNVAccelParentVTable + 340, callback->orgIOGraphicsAccelVTable + 340, sizeof(mach_vm_address_t));
+        }
     }
 }
 
