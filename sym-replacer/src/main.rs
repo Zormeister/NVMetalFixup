909a57bfed4ce6a480269c99baf5b852b4bceecc
Visual Ehrmanntraut <30368284+ChefKissInc@users.noreply.github.com>
2023-04-26 21:01:30 +0300

    Move symbol map to a RON file
    Signed-off-by: Visual Ehrmanntraut <30368284+ChefKissInc@users.noreply.github.com>

--- sym-replacer/src/main.rs
+++ sym-replacer/src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use goblin::mach::{load_command::CommandVariant, symbols::Nlist64};
+use serde::Deserialize;
 
 #[macro_use]
 extern crate log;
@@ -16,13 +17,25 @@ fn copy_dir_all(src: impl AsRef<std::path::Path>, dst: impl AsRef<std::path::Pat
     }
 }
 
+#[derive(Deserialize)]
+struct Data {
+    pub name: String,
+    pub orig2fixed: HashMap<String, String>,
+}
+
 fn main() {
     simple_logger::init().unwrap();
 
     let Some(kext_path) = std::env::args().nth(1).map(std::path::PathBuf::from) else {
-        error!("Usage: {} <kext_path>", std::env::args().next().unwrap());
+        error!("Usage: {} <kext_path> <data>", std::env::args().next().unwrap());
         return;
     };
+    let Some(data_path) = std::env::args().nth(2).map(std::path::PathBuf::from) else {
+        error!("Usage: {} <kext_path> <data>", std::env::args().next().unwrap());
+        return;
+    };
+    let data: Data = ron::from_str(&std::fs::read_to_string(data_path).unwrap()).unwrap();
+    info!("{}", data.name);
     let kext_name = kext_path.file_stem().unwrap();
     let exec_data =
         std::fs::read(kext_path.join("Contents").join("MacOS").join(kext_name)).unwrap();
@@ -42,49 +55,6 @@ fn main() {
         })
         .unwrap();
 
-    let orig2fixed = HashMap::from([
-        ("__ZN14IOAccelShared217lookupDeviceShmemEj", "__ZNK14IOAccelShared217lookupDeviceShmemEj"),
-        ("__ZN15IOAccelContext215contextModeBitsEv", "__ZNK15IOAccelContext215contextModeBitsEv"),
-        ("__ZN15IOAccelContext216isTripleBufferedEv", "__ZNK15IOAccelContext216isTripleBufferedEv"),
-        ("__ZN15IOAccelContext217getSurfaceReqBitsEv", "__ZNK15IOAccelContext217getSurfaceReqBitsEv"),
-        ("__ZN15IOAccelContext219allowsExclusiveModeEv", "__ZNK15IOAccelContext219allowsExclusiveModeEv"),
-        ("__ZN15IOAccelContext220requiresBackingStoreEv", "__ZNK15IOAccelContext220requiresBackingStoreEv"),
-        ("__ZN15IOAccelSurface216getSurfaceBufferE20eIOAccelGLBufferType", "__ZNK15IOAccelSurface216getSurfaceBufferE20eIOAccelGLBufferType"),
-        ("__ZN15IOAccelSurface227convertGLIndexToBufferIndexE20eIOAccelGLBufferType", "__ZNK15IOAccelSurface227convertGLIndexToBufferIndexE20eIOAccelGLBufferType"),
-        ("__ZN16IOAccelResource213requirePageonEv", "__ZNK16IOAccelResource213requirePageonEv"),
-        ("__ZN16IOAccelResource222newResourceWithOptionsEP22IOGraphicsAccelerator2P14IOAccelShared215eIOAccelResTypeyjyPy", "__ZN16IOAccelResource222newResourceWithOptionsEP22IOGraphicsAccelerator2P14IOAccelShared215eIOAccelResTypeyjyPyj"),
-        ("__ZN16IOAccelResource224getClientSharedROPrivateEv", "__ZNK16IOAccelResource224getClientSharedROPrivateEv"),
-        ("__ZN16IOAccelResource224getClientSharedRWPrivateEv", "__ZNK16IOAccelResource224getClientSharedRWPrivateEv"),
-        ("__ZN16IOAccelSysMemory12getDirtySizeEv", "__ZNK16IOAccelSysMemory12getDirtySizeEv"),
-        ("__ZN16IOAccelSysMemory15getResidentSizeEv", "__ZNK16IOAccelSysMemory15getResidentSizeEv"),
-        ("__ZN16IOAccelVidMemory12getDirtySizeEv", "__ZNK16IOAccelVidMemory12getDirtySizeEv"),
-        ("__ZN16IOAccelVidMemory15getResidentSizeEv", "__ZNK16IOAccelVidMemory15getResidentSizeEv"),
-        ("__ZN17IOAccel2DContext217getSurfaceReqBitsEv", "__ZNK17IOAccel2DContext217getSurfaceReqBitsEv"),
-        ("__ZN17IOAccel2DContext219allowsExclusiveModeEv", "__ZNK17IOAccel2DContext219allowsExclusiveModeEv"),
-        ("__ZN17IOAccelGLContext215contextModeBitsEv", "__ZNK17IOAccelGLContext215contextModeBitsEv"),
-        ("__ZN17IOAccelGLContext216isTripleBufferedEv", "__ZNK17IOAccelGLContext216isTripleBufferedEv"),
-        ("__ZN17IOAccelGLContext217getSurfaceReqBitsEv", "__ZNK17IOAccelGLContext217getSurfaceReqBitsEv"),
-        ("__ZN17IOAccelGLContext219allowsExclusiveModeEv", "__ZNK17IOAccelGLContext219allowsExclusiveModeEv"),
-        ("__ZN17IOAccelGLContext220requiresBackingStoreEv", "__ZNK17IOAccelGLContext220requiresBackingStoreEv"),
-        ("__ZN18IOAccelStatistics227_RESERVEDIOAccelStatistics0Ev", "__ZN18IOAccelStatistics216setupIOReportersEv"),
-        ("__ZN18IOAccelStatistics227_RESERVEDIOAccelStatistics1Ev", "__ZN18IOAccelStatistics213setupChannelsEv"),
-        ("__ZN18IOAccelStatistics227_RESERVEDIOAccelStatistics2Ev", "__ZN18IOAccelStatistics215configureReportEP19IOReportChannelListjPvS2_"),
-        ("__ZN18IOAccelStatistics227_RESERVEDIOAccelStatistics3Ev", "__ZN18IOAccelStatistics212updateReportEP19IOReportChannelListjPvS2_"),
-        ("__ZN19IOAccelCommandQueue19retireCommandBufferEP17IOAccelBlockFence", "__ZN19IOAccelCommandQueue19retireCommandBufferEP17IOAccelEventFence"),
-        ("__ZN19IOAccelCommandQueue29_RESERVEDIOAccelCommandQueue1Ev", "__ZN19IOAccelCommandQueue21processKernelCommandsEPK20IOAccelKernelCommandS2_"),
-        ("__ZN19IOAccelCommandQueue29_RESERVEDIOAccelCommandQueue2Ev", "__ZN19IOAccelCommandQueue20processKernelCommandEPK20IOAccelKernelCommandS2_"),
-        ("__ZN19IOAccelDisplayPipe214getFramebufferEv", "__ZNK19IOAccelDisplayPipe214getFramebufferEv"),
-        ("__ZN22IOAccelDisplayMachine214getDisplayPipeEj", "__ZNK22IOAccelDisplayMachine214getDisplayPipeEj"),
-        ("__ZN22IOAccelDisplayMachine216getIOFramebufferEj", "__ZNK22IOAccelDisplayMachine216getIOFramebufferEj"),
-        ("__ZN22IOAccelDisplayMachine217getScanoutSurfaceEj", "__ZNK22IOAccelDisplayMachine217getScanoutSurfaceEj"),
-        ("__ZN22IOAccelDisplayMachine218getScanoutResourceEjj", "__ZNK19IOAccelDisplayPipe218getScanoutResourceEj"),
-        ("__ZN22IOAccelDisplayMachine219getFramebufferCountEv", "__ZNK22IOAccelDisplayMachine219getFramebufferCountEv"),
-        ("__ZN22IOAccelDisplayMachine219isFramebufferActiveEj", "__ZNK22IOAccelDisplayMachine219isFramebufferActiveEj"),
-        ("__ZN22IOAccelDisplayMachine220getFullScreenSurfaceEj", "__ZNK22IOAccelDisplayMachine220getFullScreenSurfaceEj"),
-        ("__ZN22IOAccelDisplayMachine222getFramebufferResourceEjj", "__ZNK22IOAccelDisplayMachine222getFramebufferResourceEjj"),
-        ("__ZN22IOGraphicsAccelerator231_RESERVEDIOGraphicsAccelerator0Ev", "__ZN22IOGraphicsAccelerator218getMaxResourceSizeEv"),
-    ]);
-
     let mut new_str_table = vec![0u8; 4];
     let mut off2fixed = HashMap::new();
     for (sym, off) in macho
@@ -93,7 +63,7 @@ fn main() {
         .map(|(sym, nlist)| (sym, nlist.n_strx))
     {
         off2fixed.insert(off, new_str_table.len());
-        let v = *orig2fixed.get(sym).unwrap_or(&sym);
+        let v = data.orig2fixed.get(sym).map(|v| v.as_str()).unwrap_or(sym);
         if sym != v {
             info!("{sym} -> {v}");
         }
